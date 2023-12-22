a421651a8f1eae18a44b89291106df2a772d3ecb
Chinmay Bhosale <chinmayhbhosale02@gmail.com>
2023-12-22 19:28:49 +0530

    all_configs

---
+++ config/fish/config.fish
@@ -0,0 +1,10 @@
+if status is-interactive
+
+
+    
+
+  #                                                      blur_terminal_background  
+    # Commands to run in interactive sessions can go here
+end
+starship init fish | source
+
---
+++ config/fish/functions/sl.fish
@@ -0,0 +1,4 @@
+function sl --description 'alias sl=sl | lolcat'
+ command sl | lolcat $argv
+        
+end
